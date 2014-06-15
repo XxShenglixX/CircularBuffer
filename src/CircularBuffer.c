904fd3515f6c113389e711b1021fe1b0cf10cb70
XxShenglixX <c_shengli@hotmail.com>
2014-06-15 22:00:53 +0800

    Complete circularBufferRemove(), added test code and comments

--- src/CircularBuffer.c
+++ src/CircularBuffer.c
@@ -14,6 +14,12 @@
 }
  */
  
+/* Initialise the circular buffer
+ *
+ * Input :
+ *	length is the maximum of data that can be allocated in the buffer	
+ */
+ 
 CircularBuffer *circularBufferNew(int length)
 {
 	CircularBuffer *circularBuffer;
@@ -28,44 +34,78 @@ CircularBuffer *circularBufferNew(int length)
 	return circularBuffer;
 }
 
+/* Free the memory
+ * 
+ */
 void circularBufferDel(CircularBuffer *circularBuffer)
 {
 	free(circularBuffer);
 }
 
+/* *
+ * Add data to the circular buffer 
+ * Note : Will throw an error if the buffer is full
+ *
+ * Input : 
+ *	cb is the circular buffer
+ *	value is the data needed to be written
+ *
+ * Precondition :
+ *	size must not be negative
+ */
 void circularBufferAdd(CircularBuffer *cb,int value)
 {
-	if (cb->size == cb->length)
-	{
+	if (cb->size >= cb->length)	// if size of the buffer is larger or equal to the length of the buffer, the buffer is considered full and will
+	{							// throw an error
 		Throw(ERR_BUFFER_IS_FULL);
 	}
 
-	cb->head = cb->buffer;
+	cb->head = cb->buffer; // reset head to the starting location of buffer for later use
 		
-	if (cb->size != 0) 
+	if (cb->size != 0)  // if the buffer already contains data,
 		{
-			cb->head += (cb->size);
-			cb->tail = cb->head ;
-			cb->tail -- ;
+			cb->head += (cb->size);	 //move head to last written location + 1 by using the size of buffer to determine
+			cb->tail = cb->head ; 	 // move tail to head and then  
+			cb->tail -- ;			// decrement once
 		}
 		
-	*cb->head = value ;
-	cb->head ++;
+	*cb->head = value ; 	// load the head with value
 	
-	if (cb->size > 0)
+	if (cb->size > 0)		//tail will only increment if size is greater than 0 to make sure tail is always 1 step behind head 
 		cb->tail ++;
 		
-	cb->size +=1 ;
+	cb->size +=1 ;	// increase the size of buffer
 	
 	
 }
 
 
-
+/** 
+ * Retrieve the last data in the circular buffer and allow the location to be overwritten with new data
+ * Note : will throw an error if the buffer is empty
+ *
+ * Input : 
+ *	cb is the circular buffer
+ * Return :
+ * value is the data contain in the last location of the circular buffer
+ */
 int circularBufferRemove(CircularBuffer *cb)
 {
-	if (cb->size == 0)
+	if (cb->size == 0) // the buffer is empty if the size is 0 and will throw an error
 	{
 		Throw(ERR_BUFFER_IS_EMPTY);
 	}
+	
+
+	int value ; // use to retrieve data in the buffer
+
+	value = *cb->tail ; // load value with data in the tail 
+	cb->size -- ; 	// decrease size of buffer
+	
+	if (cb->size != 0) //will stop decrement tail is size is 0 to prevent over decrement
+	{
+		cb->tail -- ;
+	}
+	
+	return value; // returns the value retrieved
 }
--- test/test_CircularBuffer.c
+++ test/test_CircularBuffer.c
@@ -22,6 +22,7 @@ void test_CircularBufferNew_given_6_should_allotcate_CircularBuffer_obeject_with
 	circularBufferDel(cb);
 }
 
+
 void test_circularBufferRemove_empty_buffer_should_throw_an_error()
 {
 	CircularBuffer *cb = circularBufferNew(6);
@@ -106,3 +107,149 @@ void test_circularBufferAdd_bufferof_given_length_5_value_1_2_3_4_5_6_should_ret
 	}
 	circularBufferDel(cb);
 }
+
+
+void test_circularBufferRemove_removing_buffer_containing_1_2_3_4_5_6_should_return_6_5_4_3_2_1()
+{
+	CircularBuffer *cb = circularBufferNew(6);
+	CEXCEPTION_T err;
+	
+	Try
+	{
+		circularBufferAdd(cb,1);
+		circularBufferAdd(cb,2);
+		circularBufferAdd(cb,3);
+		circularBufferAdd(cb,4);
+		circularBufferAdd(cb,5);
+		circularBufferAdd(cb,6);
+		
+		
+		TEST_ASSERT_EQUAL(6,circularBufferRemove(cb));
+		TEST_ASSERT_EQUAL(5,circularBufferRemove(cb));
+		TEST_ASSERT_EQUAL(4,circularBufferRemove(cb));
+		TEST_ASSERT_EQUAL(3,circularBufferRemove(cb));
+		TEST_ASSERT_EQUAL(2,circularBufferRemove(cb));
+		TEST_ASSERT_EQUAL(1,circularBufferRemove(cb));
+		
+		printf("Success: No exception generated.\n");
+
+	}
+	Catch(err)
+	{
+		TEST_FAIL_MESSAGE("Do not expect exception to be generated.\n");
+	}
+	circularBufferDel(cb);
+}
+
+void test_circularBufferRemove_removing_buffer_containing_1_2_3_4_5_6_should_return_6_5_4_3_2_1_and_throw_an_error()
+{
+	CircularBuffer *cb = circularBufferNew(6);
+	CEXCEPTION_T err;
+	
+	Try
+	{
+		circularBufferAdd(cb,1);
+		circularBufferAdd(cb,2);
+		circularBufferAdd(cb,3);
+		circularBufferAdd(cb,4);
+		circularBufferAdd(cb,5);
+		circularBufferAdd(cb,6);
+		
+		
+		TEST_ASSERT_EQUAL(6,circularBufferRemove(cb));
+		TEST_ASSERT_EQUAL(5,circularBufferRemove(cb));
+		TEST_ASSERT_EQUAL(4,circularBufferRemove(cb));
+		TEST_ASSERT_EQUAL(3,circularBufferRemove(cb));
+		TEST_ASSERT_EQUAL(2,circularBufferRemove(cb));
+		TEST_ASSERT_EQUAL(1,circularBufferRemove(cb));
+		circularBufferRemove(cb);
+		printf("Error should generate an exception due to empty buffer\n");
+
+	}
+	Catch(err)
+	{
+		TEST_ASSERT_EQUAL(ERR_BUFFER_IS_EMPTY,err);
+		printf("Error code [%d] Buffer is empty\n",ERR_BUFFER_IS_EMPTY);
+	}
+	circularBufferDel(cb);
+}
+
+void test_circularBufferRemove_and_Add_removing_buffer_containing_1_2_3_4_5_6_and_add_11_12_13_14_15_16_should_return_11_12_13_14_15_16()
+{
+	CircularBuffer *cb = circularBufferNew(6);
+	CEXCEPTION_T err;
+	
+	Try
+	{
+		circularBufferAdd(cb,1);
+		circularBufferAdd(cb,2);
+		circularBufferAdd(cb,3);
+		circularBufferAdd(cb,4);
+		circularBufferAdd(cb,5);
+		circularBufferAdd(cb,6);
+		
+		circularBufferRemove(cb);
+		circularBufferRemove(cb);
+		circularBufferRemove(cb);
+		circularBufferRemove(cb);
+		circularBufferRemove(cb);
+		circularBufferRemove(cb);
+		
+		circularBufferAdd(cb,11);
+		TEST_ASSERT_EQUAL(11,*cb->tail);
+		circularBufferAdd(cb,12);
+		TEST_ASSERT_EQUAL(12,*cb->tail);
+		circularBufferAdd(cb,13);
+		TEST_ASSERT_EQUAL(13,*cb->tail);
+		circularBufferAdd(cb,14);
+		TEST_ASSERT_EQUAL(14,*cb->tail);
+		circularBufferAdd(cb,15);
+		TEST_ASSERT_EQUAL(15,*cb->tail);
+		circularBufferAdd(cb,16);
+		TEST_ASSERT_EQUAL(16,*cb->tail);
+		
+		printf("Success: No exception generated.\n");
+
+	}
+	Catch(err)
+	{
+		TEST_FAIL_MESSAGE("Do not expect exception to be generated.\n");
+	}
+	circularBufferDel(cb);
+}
+
+void test_circularBufferRemove_and_Add_remove_3_4_for_buffer_containing_1_2_3_4_and_add_5_6_should_return_1_2_5_6()
+{
+	CircularBuffer *cb = circularBufferNew(6);
+	CEXCEPTION_T err;
+	
+	Try
+	{
+		circularBufferAdd(cb,1);
+		circularBufferAdd(cb,2);
+		circularBufferAdd(cb,3);
+		circularBufferAdd(cb,4);
+		
+		
+		circularBufferRemove(cb);
+		circularBufferRemove(cb);
+		
+		TEST_ASSERT_EQUAL(1,*cb->buffer);
+		
+		TEST_ASSERT_EQUAL(2,*(cb->buffer+1));
+		circularBufferAdd(cb,5);
+		TEST_ASSERT_EQUAL(5,*cb->tail);
+		circularBufferAdd(cb,6);
+		TEST_ASSERT_EQUAL(6,*cb->tail);
+
+		
+		printf("Success: No exception generated.\n");
+
+	}
+	Catch(err)
+	{
+		TEST_FAIL_MESSAGE("Do not expect exception to be generated.\n");
+	}
+	circularBufferDel(cb);
+}
+

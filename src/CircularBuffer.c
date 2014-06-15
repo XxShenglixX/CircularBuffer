592a64dc0d15ef0cc1d768083e143d0909c89305
XxShenglixX <c_shengli@hotmail.com>
2014-06-15 19:37:35 +0800

    Added circularBufferAdd() ,incomplete circularBufferRemove() and test code

--- src/CircularBuffer.c
+++ src/CircularBuffer.c
@@ -32,3 +32,40 @@ void circularBufferDel(CircularBuffer *circularBuffer)
 {
 	free(circularBuffer);
 }
+
+void circularBufferAdd(CircularBuffer *cb,int value)
+{
+	if (cb->size == cb->length)
+	{
+		Throw(ERR_BUFFER_IS_FULL);
+	}
+
+	cb->head = cb->buffer;
+		
+	if (cb->size != 0) 
+		{
+			cb->head += (cb->size);
+			cb->tail = cb->head ;
+			cb->tail -- ;
+		}
+		
+	*cb->head = value ;
+	cb->head ++;
+	
+	if (cb->size > 0)
+		cb->tail ++;
+		
+	cb->size +=1 ;
+	
+	
+}
+
+
+
+int circularBufferRemove(CircularBuffer *cb)
+{
+	if (cb->size == 0)
+	{
+		Throw(ERR_BUFFER_IS_EMPTY);
+	}
+}
--- test/test_CircularBuffer.c
+++ test/test_CircularBuffer.c
@@ -1,5 +1,6 @@
 #include "unity.h"
 #include "CircularBuffer.h"
+#include "CException.h"
 
 void setUp(void)
 {
@@ -9,7 +10,7 @@ void tearDown(void)
 {
 }
 
-void test_circularBufferNew_given_6_should_allotcate_CircularBuffer_obeject_with_a_buffer_of_6(void)
+void test_CircularBufferNew_given_6_should_allotcate_CircularBuffer_obeject_with_a_buffer_of_6(void)
 {
 	CircularBuffer *cb = circularBufferNew(6);
 	TEST_ASSERT_NOT_NULL(cb);
@@ -18,5 +19,90 @@ void test_circularBufferNew_given_6_should_allotcate_CircularBuffer_obeject_with
 	TEST_ASSERT_EQUAL(0,cb->size);
 	TEST_ASSERT_EQUAL_PTR(cb->buffer,cb->head);
 	TEST_ASSERT_EQUAL_PTR(cb->buffer,cb->tail);
+	circularBufferDel(cb);
+}
+
+void test_circularBufferRemove_empty_buffer_should_throw_an_error()
+{
+	CircularBuffer *cb = circularBufferNew(6);
+	CEXCEPTION_T err;
+	
+	Try
+	{
+		circularBufferRemove(cb);
+		printf("Error should generate an exception due to empty buffer\n");
+	}
+	Catch(err)
+	{
+		TEST_ASSERT_EQUAL(ERR_BUFFER_IS_EMPTY,err);
+		printf("Error code [%d] Buffer is empty\n",ERR_BUFFER_IS_EMPTY);
+	}
+	circularBufferDel(cb);
+}
+
+void test_circularBufferAdd_bufferof_length_6_size_6_should_throw_an_error()
+{
+	CircularBuffer *cb = circularBufferNew(6);
+	CEXCEPTION_T err;
+	cb->size = 6;
+	
+	Try
+	{
+		circularBufferAdd(cb,0);
+		printf("Error should generate an exception due to full buffer\n");
+	}
+	Catch(err)
+	{
+		TEST_ASSERT_EQUAL(ERR_BUFFER_IS_FULL,err);
+		printf("Error code [%d] Buffer is full\n",ERR_BUFFER_IS_FULL);
+	}
+	circularBufferDel(cb);
+}  
+
+
+void test_circularBufferAdd_bufferof_given_length_6_value_5_should_return_5_for_the_1st_data_in_buffer()
+{
+	CircularBuffer *cb = circularBufferNew(6);
+	CEXCEPTION_T err;
+	
+	Try
+	{
+		circularBufferAdd(cb,5);
+		printf("Success: No exception generated.\n");
+		TEST_ASSERT_EQUAL(5,*cb->tail);
+	}
+	Catch(err)
+	{
+		TEST_FAIL_MESSAGE("Do not expect exception to be generated.\n");
+	}
+	circularBufferDel(cb);
+}
+
+void test_circularBufferAdd_bufferof_given_length_5_value_1_2_3_4_5_6_should_return_1_2_3_4_5_and_throw_an_error()
+{
+	CircularBuffer *cb = circularBufferNew(5);
+	CEXCEPTION_T err;
+	
+	Try
+	{
+		circularBufferAdd(cb,1);
+		TEST_ASSERT_EQUAL(1,*cb->tail);
+		circularBufferAdd(cb,2);
+		TEST_ASSERT_EQUAL(2,*cb->tail);
+		circularBufferAdd(cb,3);
+		TEST_ASSERT_EQUAL(3,*cb->tail);
+		circularBufferAdd(cb,4);
+		TEST_ASSERT_EQUAL(4,*cb->tail);
+		circularBufferAdd(cb,5);
+		TEST_ASSERT_EQUAL(5,*cb->tail);
+		circularBufferAdd(cb,6);
+		printf("Error should generate an exception due to full buffer\n");
+	}
+	Catch(err)
+	{
+		TEST_ASSERT_EQUAL(ERR_BUFFER_IS_FULL,err);
+		printf("Error code [%d] Buffer is full\n",ERR_BUFFER_IS_FULL);
+	
+	}
 	circularBufferDel(cb);
 }
